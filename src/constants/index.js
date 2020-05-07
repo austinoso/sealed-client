1f02bef49466b0e8bc7a7d4fba298ae8f4229109
austinoso <osorio_austin@yahoo.com>
2020-05-06 20:03:26 -0700

    added heroku link

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,7 @@
 import actionCable from 'actioncable';
 
-export const API_ROOT = 'http://localhost:3000';
-export const API_WS_ROOT = 'ws://localhost:3000/cable';
+export const API_ROOT = 'https://sealed-api.herokuapp.com/';
+export const API_WS_ROOT = 'ws://https://sealed-api.herokuapp.com//cable';
 export const HEADERS = {
 	'Content-Type': 'application/json',
 	Accept: 'application/json',
